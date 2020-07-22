b581498fc1e37b65edc2b94bc292b1b9485bdeda
Nhat Le <NhatL@users.noreply.github.com>
2020-07-22 11:25:20 -0400

    Dont remove cookies

--- server.js
+++ server.js
@@ -26,8 +26,6 @@ cors_proxy.createServer({
   requireHeader: ['origin', 'x-requested-with'],
   checkRateLimit: checkRateLimit,
   removeHeaders: [
-    'cookie',
-    'cookie2',
     // Strip Heroku-specific headers
     'x-heroku-queue-wait-time',
     'x-heroku-queue-depth',
